782acc242ea908e45c88f786d88aa7b1be8d9fe6
Ada Cooke <me@ada.rip>
2022-06-24 04:52:16 +0100

    Initial commit

---
+++ src/main/java/gg/nodus/fakereport/Fakereport.java
@@ -0,0 +1,17 @@
+package gg.nodus.fakereport;
+
+import com.mojang.authlib.minecraft.report.ReportChatMessage;
+import net.fabricmc.api.ModInitializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fakereport implements ModInitializer {
+
+    public static final List<String> context = new ArrayList<>();
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/gg/nodus/fakereport/client/FakereportClient.java
@@ -0,0 +1,13 @@
+package gg.nodus.fakereport.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class FakereportClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
